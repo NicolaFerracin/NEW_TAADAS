717381eb02098258559e2b4beaaf21fc6d3a2901
Una Gauper <umgauper@gmail.com>
2016-06-19 14:25:18 -0700

    refactor publication orders table, and publication order page

--- public/js/requests_form.js
+++ public/js/requests_form.js
@@ -1,25 +1,56 @@
 $(function() {
 	
-	var items = JSON.parse(sessionStorage.getItem('requestedItems'));
-	console.log('session storage returns: ', items, 'type: ', typeof(items));
-
-	var quantities = JSON.parse(sessionStorage.getItem('quantities'));
+	var items = JSON.parse(sessionStorage.getItem('orderedItems'));
 
 	items.forEach(function(el) {
-		$('#requested-items').append('<tr><td>' + el.title + '</td>' + '<td class="quantity"><input type="text" value=' + quantities[el.identifier] + '></input></td><td><button class="delete">X</button></td></tr>');
+		$('#requested-items').append('<tr><td class="title">' + el.title + '</td>' + '<td class="quantity"><input type="number" min="1" max="100" value=' + el.qty + '></input></td><td class="identifier">' + el.identifier + '</td><td><button class="delete">X</button></td></tr>');
 
 	});
 
 	$('.delete').on('click', function(e) {
 		e.target.parentElement.parentElement.remove();
 	});
-	
-	
+
+
+
 	
 $('#submititems').on('click', function(e){	
+	e.preventDefault();
+
+	function addSpaces(spaces, str) {
+			while (spaces > 0) {
+				str += ' ';
+				spaces--;
+			}
+
+			return str;
+		}
 
 	var data = $('#publications-order').serialize();
 	data = data.replace(/\+/g, ' ').replace(/\=/g, ': ').replace(/&/g, '\r\n');
+	data += '\r\n';
+
+	// add list of ordered titles, identifiers, and quantity to data
+
+	var rows = $('#requested-items > tr');
+
+	rows.each(function(index, row) {
+		var title = $(this).find('td.title').text();
+		var quantity = $(this).find('td.quantity > input').val();
+		var identifier = $(this).find('td.identifier').text();
+
+		var spaceAfterIdentifier = 50 - identifier.length;
+		var spaceAfterTitle = 100 - title.length;
+		var spaceAfterQuantity = 30 - quantity.length;
+
+		var newStr = addSpaces(spaceAfterIdentifier, identifier);
+		newStr += addSpaces(spaceAfterTitle, title);
+		newStr += addSpaces(spaceAfterQuantity, quantity);
+
+		data = newStr + '\r\n' + data;
+	});
+
+	console.log(data);
 
 	$.ajax({
                 url: "../order",
@@ -27,7 +58,7 @@ $('#submititems').on('click', function(e){
                 data: data,
                 cache: false,
                 success: function() {
-                	// change to redirect to a success message
+                	// change to redirect to a success message page
 					console.log("success");
                 },
                 error: function() {
---
+++ public/js/structure after adding pdf.js
@@ -0,0 +1,101 @@
+{
+	"_id" : ObjectId("575b47385a0bc2f1a84f8009"),
+	"identifier" : 0,
+	"title" : "\"Light\" Cigarettes and Cancer Risk - Fact Sheet",
+	"tags" : [
+		"tobacco/nicotine"
+	],
+	"description" : "Answers questions about the health effects of smoking low-tar, mild, or light cigarettes. National Cancer Institute Fact Sheet",
+	"pLongdescription" : "Answers questions about the health effects of smoking low-tar, mild, or light cigarettes. National Cancer Institute Fact Sheet  \"Fact sheet image\" prodimages/Smokeless Tobacco and Cancer Questions and Answers.jpg",
+	"pImage" : "http://i1283.photobucket.com/albums/a551/taadasphotos/Literature%20covers/LightCigarettes_web_zps998035f9.jpg",
+	"pLargeimage" : "prodimages/light cigs.pdf",
+	"pDisplay" : 1,
+	"pSell" : 1,
+	"pExemptions" : 0,
+	"pInStock" : 848,
+	"" : "",
+	"type" : "publication",
+	"createdAt" : "2016-06-11",
+	"published" : true,
+	"hideTitle" : false,
+	"thumbnail" : {
+		"items" : [ ],
+		"type" : "area"
+	},
+	"body" : {
+		"items" : [
+			{
+				"editView" : "1",
+				"widget" : true,
+				"type" : "files",
+				"id" : "w47470786176735808",
+				"ids" : [
+					"520137480101062540"
+				],
+				"extras" : {
+					"520137480101062540" : {
+						"hyperlink" : null,
+						"hyperlinkTitle" : "",
+						"hyperlinkTarget" : false
+					}
+				},
+				"showTitles" : false,
+				"showDescriptions" : false,
+				"showCredits" : false
+			},
+			{
+				"widget" : true,
+				"editView" : "1",
+				"type" : "slideshow",
+				"id" : "w376295507862340136",
+				"ids" : [
+					"420845104834805314"
+				],
+				"extras" : {
+					"420845104834805314" : {
+						"hyperlink" : null,
+						"hyperlinkTitle" : "",
+						"hyperlinkTarget" : false
+					}
+				},
+				"showTitles" : false,
+				"showDescriptions" : false,
+				"showCredits" : false
+			}
+		],
+		"type" : "area"
+	},
+	"physical" : true,
+	"slug" : "light-cigarettes-and-cancer-risk-fact-sheet",
+	"sortTitle" : "light cigarettes and cancer risk fact sheet",
+	"publishedAt" : "2016-06-11",
+	"highSearchText" : "light cigarettes and cancer risk fact sheet undefined light cigarettes and cancer risk fact sheet tobacco nicotine light cigarettes and cancer risk fact sheet answers questions about the health effects of smoking low tar mild or light cigarettes national cancer institute fact sheet",
+	"highSearchWords" : [
+		"light",
+		"cigarettes",
+		"and",
+		"cancer",
+		"risk",
+		"fact",
+		"sheet",
+		"undefined",
+		"tobacco",
+		"nicotine",
+		"answers",
+		"questions",
+		"about",
+		"the",
+		"health",
+		"effects",
+		"of",
+		"smoking",
+		"low",
+		"tar",
+		"mild",
+		"or",
+		"national",
+		"institute"
+	],
+	"lowSearchText" : "light cigarettes and cancer risk fact sheet undefined light cigarettes and cancer risk fact sheet tobacco nicotine light cigarettes and cancer risk fact sheet answers questions about the health effects of smoking low tar mild or light cigarettes national cancer institute fact sheet",
+	"searchSummary" : ""
+}
