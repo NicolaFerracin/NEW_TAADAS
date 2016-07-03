1abae4a8c39f97d9c060bd3d855039ffd1244f5f
unknown <naiyucko@yahoo.com>
2016-07-03 14:11:40 -0700

    Revert wrong changes

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ site.init({
   shortName: 'NEW_TAADAS',
   hostName: 'NEW_TAADAS',
   title: 'NEW_TAADAS',
-  sessionSecret: 'test',
-  adminPassword: 'demo',
+  sessionSecret: process.env.SESSION_SECRET,
+  adminPassword: process.env.PASSWORD,
   address: '0.0.0.0',
   port: 3000,
 
@@ -174,7 +174,7 @@ site.init({
       ]   
     }
   },
-/*
+
     uploadfs: {
       backend: 's3',
       secret: process.env.AMAZON_SECRET,
@@ -182,7 +182,7 @@ site.init({
       bucket: 'taadas',
       region: 'us-west-1'
     },
-*/
+
   // These are assets we want to push to the browser.
   // The scripts array contains the names of JS files in /public/js,
   // while stylesheets contains the names of LESS files in /public/css
